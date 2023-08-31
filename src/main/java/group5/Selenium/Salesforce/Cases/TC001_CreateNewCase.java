9d1d6c3bc542ebffcd3c37cff28af4c251dbd8be
sathishkumaravelu <sathishkumaravelu@gmail.com>
2023-08-31 15:40:15 +0530

    correction to file naming and optimizaton

--- src/main/java/group5/Selenium/Salesforce/Cases/TC001_CreateNewCase.java
+++ src/main/java/group5/Selenium/Salesforce/Cases/TC001_CreateNewCase.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class TC001_CreateNewCase {
@@ -28,20 +29,23 @@ public class TC001_CreateNewCase {
 		9) Enter Subject as 'Testing' and description as 'Dummy'
 		10) Click 'Save' and verify the message
 	 */
+	public WebDriverWait wait;
+	public ChromeDriver driver;
+	
 	
 	@Test
 	public void tc001_CreateNewCase() {
 		ChromeOptions op = new ChromeOptions();
 		op.addArguments("--disable-notifications");
-		ChromeDriver driver = new ChromeDriver(op);
+		driver = new ChromeDriver(op);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-		
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		driver.get("https://sdet3-dev-ed.develop.my.salesforce.com/");
 		driver.findElement(By.id("username")).sendKeys("sathishsdet95@2023.com");
 		driver.findElement(By.id("password")).sendKeys("iamgoingtobeSDET2023");
 		driver.findElement(By.id("Login")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[text()='Setup Home']"))));
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='View All Applications']"))).click();
 		driver.findElement(By.xpath("//p[text()='Sales']")).click();
@@ -50,34 +54,27 @@ public class TC001_CreateNewCase {
 		wait.until(ExpectedConditions.elementToBeClickable(accountsElement));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].click();",accountsElement);
-		
-		driver.findElement(By.xpath("(//*[@data-id='Case'])[last()]")).click();
-		
+		driver.findElement(By.xpath("(//*[@data-id='Case'])[last()]")).click();	
 		driver.findElement(By.xpath("//a[@title='New']")).click();
-		
 		driver.findElement(By.xpath("//*[@placeholder='Search Contacts...']")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@title='Morkus tomas']"))).click();		
-		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@title='Morkus tomas']"))).click();			
 		driver.findElement(By.xpath("(//label[text()='Case Origin']/following-sibling::div)[1]//button")).click();
-		driver.findElement(By.xpath("//span[@title='Email']")).click();		
-		
-		
-				
+		driver.findElement(By.xpath("//span[@title='Email']")).click();				
 		driver.findElement(By.xpath("(//label[text()='Status']/following-sibling::div)[1]//button")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@title='Escalated']"))).click();		
-		
-		
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@title='Escalated']"))).click();				
 		driver.findElement(By.xpath("//*[@name='Subject']")).sendKeys("Testing");
 		driver.findElement(By.xpath("(//*[text()='Description']/following::textarea)[1]")).sendKeys("Dummy");
-		
 		driver.findElement(By.xpath("//button[@name='SaveEdit']")).click();
-		
 		String caseNumber = driver.findElement(By.xpath("//span[@data-aura-class='forceActionsText']//a")).getAttribute("title");
 		String expectedNumber = driver.findElement(By.xpath("(//p[@title='Case Number']/following::p)[1]")).getText();		
-		
 		Assert.assertEquals(caseNumber, expectedNumber,"case number not created");
 		
-		//boolean displayed = driver.findElement(By.xpath("")).isDisplayed();
+	}
+	
+	@AfterTest
+	public void afterSetup() {
+		
+		driver.close();
 	}
 
 }
--- src/main/java/group5/Selenium/Salesforce/WorkType/TC003_EditWorkTypePositive.java
+++ src/main/java/group5/Selenium/Salesforce/WorkType/TC003_EditWorkTypePositive.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class TC003_EditWorkTypePositive {
@@ -32,21 +33,25 @@ public class TC003_EditWorkTypePositive {
 		12) Verify the error message
 		
 	 */
+	public WebDriverWait wait;
+	public ChromeDriver driver;
+
 	
-	@Test
+	@Test(dependsOnMethods = "group5.Selenium.Salesforce.WorkType.TC002_EditWorkTypeNegative.tc002_EditWorkTypeNegative")
 	public void tc003_EditWorkTypePositive() {
 		ChromeOptions op = new ChromeOptions();
 		op.addArguments("--disable-notifications");
-		ChromeDriver driver = new ChromeDriver(op);
+		driver = new ChromeDriver(op);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		
 		driver.get("https://sdet3-dev-ed.develop.my.salesforce.com/");
 		driver.findElement(By.id("username")).sendKeys("sathishsdet95@2023.com");
 		driver.findElement(By.id("password")).sendKeys("iamgoingtobeSDET2023");
 		driver.findElement(By.id("Login")).click();
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[text()='Setup Home']"))));
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='slds-icon-waffle']"))).click();
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='View All Applications']"))).click();
 		WebElement searchEle = driver.findElement(By.xpath("//input[@part='input']"));
@@ -62,6 +67,12 @@ public class TC003_EditWorkTypePositive {
 		driver.findElement(By.xpath("//button[@title='Save']")).click();
 		boolean isDisplayed = driver.findElement(By.xpath("//span[@data-aura-class='forceActionsText']")).isDisplayed();
 		Assert.assertEquals(isDisplayed, true);
+		
 	}
 
+	@AfterTest
+	public void afterSetup() {
+		
+		driver.close();
+	}
 }
